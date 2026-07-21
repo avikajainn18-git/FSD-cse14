d70d579bd70334ae5f7d6a9c0a7899d30aa3cb40
Avika <avika.jainn18@gmail.com>
2026-07-21 10:03:10 +0530

    event-emitter

--- Lab1/eventEmitter.js
+++ Lab1/eventEmitter.js
@@ -1,23 +1,31 @@
-import {EventEmitter} from 'node:events'
-const task=new EventEmitter();
+import { EventEmitter } from "node:events";
+const task = new EventEmitter();
 
-const sayHi=(name)=>{
-    console.log(`Logged-in ${name}`);
+const sayHi = (name) => {
+  console.log(`Logged-in as ${name}`);
 };
-
-task.on('greet', sayHi);
-task.on("greet",()=> {
-        console.log("Logged Out");
-
-});
-task.once("greet",()=> {
-    console.log("Systrem restarting...");
-});
-task.emit("exit",()=>{
-    console.log(`System is shutting down`);
+task.once("greet", () => {
+  console.log("Systrem starting...");
 });
 
-task.emit('greet', 'avika jain');
-task.emit('greet', 'rahul singh');
-task.emit('greet', 'sonia singh');
-task.emit("greet", "Manager");
+task.on("greet", sayHi);
+
+task.on("greet", (name) => {
+  console.log(`${name} starts working`);
+});
+task.on("greet", (name) => {
+  console.log(`${name} logged out \n`);
+});
+
+task.once("exit", (name) => {
+  console.log(`System is shutting down by ${name}`);
+});
+
+task.emit("greet", "avika jain");
+task.off("greet", sayHi);
+task.emit("greet", "rahul singh");
+task.emit("greet", "sonia sinha");
+task.emit("exit", "Manager");
+task.emit("exit", "Employee"); //wont execute
+console.log("total listener", task.listenerCount("greet"));
+task.removeAllListeners("greet");
