9bba78e70a00ca28c2d5b75fc06de7873a7d80ad
Avika <avika.jainn18@gmail.com>
2026-07-23 15:34:35 +0530

    task1

--- Lab1/Eventloop/task1.js
+++ Lab1/Eventloop/task1.js
@@ -1,17 +1,33 @@
-const f1=()=>{
-    console.log("f1")
+import { TIMEOUT } from "node:dns";
+
+const f1 = () => {
+    console.log("f1 starts");
+    f2();
+    console.log("f1 running");
+    console.log("f1 stops");
+
 }
-const f2=()=>{
-    console.log("f2")
+const f2 = () => { 
+    console.log("f2 starts");
+    f3();
+    console.log("f2 running");
+    console.log("f2 stopd");
 }
-const f3=()=>{
-    console.log("f3")
+const f3 = () => {
+    console.log("f3 starts");
+    console.log("f3 running");
+    console.log("f3 stops");
 }
 function main() {
-    console.log("main")
+    console.log("main starts")
     f1();
-    f2();
-    f3();
-    console.log("end main");
+    console.log("main running");
+    console.log("end main"); //this is called synchronous call or single threaded execution
 }
 main();
+// In asynchronous, we use eventloop to manage the call stack.
+// Asynchronous calls using timer:
+// 1) set TIMEOUT
+// 2) Set setImmediate
+// 3) process.nextTick
+// 4) setInterval
